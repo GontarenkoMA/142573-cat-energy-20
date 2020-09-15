ab202c16e8e9291b4c9b6033843ceb0603d132d4
Maria <mary.forever@mail.ru>
2020-09-15 11:09:01 +0300

    Удалены комментарии в скрипте

--- source/js/script.js
+++ source/js/script.js
@@ -48,33 +48,15 @@ function initMap() {
     zoom: 14.25,
     disableDefaultUI:true
   });
-
   icon = {
-      // scaledSize: new google.maps.Size(50, 50),
-      // origin: new google.maps.Point(0, 0),
-      // anchor: new google.maps.Point(25, 50),
-      url: "../img/map-pin.png"
+    url: "../img/map-pin.png"
   };
-
   marker = new google.maps.Marker({
     position: { lat: 59.93838, lng: 30.3231},
     map: map,
     icon: icon
   });
 }
-//
-// function myFunction(x) {
-//     if (x.matches) { // If media query matches
-//         document.body.style.backgroundColor = "yellow";
-//     } else {
-//         document.body.style.backgroundColor = "pink";
-//     }
-// }
-//
-// var x = window.matchMedia("(max-width: 768px)");
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
-
 
 /* Проверка формы
 если есть */
